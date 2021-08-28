fb13af811d93c0f99d88b5d5aa8f4b248cd3a823
pvpatil5 <pvpatil123@yahoo.com>
2021-08-28 17:02:07 +0530

    Sample 1 added

---
+++ src/main/java/com/vtiger/generic/Sample1.java
@@ -0,0 +1,10 @@
+package com.vtiger.generic;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This ia sample 1");
+	}
+
+}
